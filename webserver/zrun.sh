071019ee3b6eb8ae45597cdb05fd56e4f97274b2
runrunliuliu <liuliu.tju@gmail.com>
2017-12-06 17:35:42 +0800

    fix

--- webserver/zrun.sh
+++ webserver/zrun.sh
@@ -3,10 +3,13 @@
 mkdir -p logs/
 
 # load stockinfo.csv
-# python main.py -m stockinfo -p ~/apps/pyalgotrade/data/
+python main.py -m stockinfo -p ~/apps/pyalgotrade/data/
 
 # load xingtai
-# python main.py -m xingtai -p ~/apps/pyalgotrade/data/dayk/xingtai/
+python main.py -m xingtai -p ~/apps/pyalgotrade/data/dayk/xingtai/
 
 # load mtime 
 python main.py -m mtime -p ~/apps/pyalgotrade/data/dayk/mtime/
+
+# load dayk
+# python main.py -m dayk -p ~/apps/dltrade/data/dayk/
