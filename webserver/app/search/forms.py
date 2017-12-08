611be9e759feebfcdf87e17edff493f599013120
runrunliuliu <liuliu.tju@gmail.com>
2017-12-08 16:32:21 +0800

    change Form to FlaskFrom

--- webserver/app/search/forms.py
+++ webserver/app/search/forms.py
@@ -1,6 +1,7 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
-class SearchForm(Form):
+
+class SearchForm(FlaskForm):
     search = StringField('Search', validators=[DataRequired()])
